011561290c0ae4bd5f2370785ea0d1ffe68267a7
shrouk <shroukeslam676@gmail.com>
2024-09-23 20:45:59 +0300

    donation history and items details

--- judur/src/app/app.routes.ts
+++ judur/src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';  
+// import { LandownerRegistrationComponent } from './landowner-registration/landowner-registration.component';
+// import { VolunteerRegistrationComponent } from './volunteer-registration/volunteer-registration.component';
+// import { AuctionComponent } from './auction/auction.component';
+// import { VolunteerToExaminerComponent } from './volunteer-to-examiner/volunteer-to-examiner.component'; 
+// import { DonationHistoryComponent } from './donation-history/donation-history.component'; 
+// import { DonationItemDetailsComponent } from './donation-item-details/donation-item-details.component';
+// import { DonationMoneyDetailsComponent } from './donation-money-details/donation-money-details.component';
+// import { DonationLandDetailsComponent } from './donation-land-details/donation-land-details.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   { path: 'landowner-registration', component: LandownerRegistrationComponent },
+//   { path: 'volunteer-registration', component: VolunteerRegistrationComponent },
+//   { path: 'auction', component: AuctionComponent },
+//   { path: 'volunteer-to-examiner', component: VolunteerToExaminerComponent },
+//   { path: 'donation-history', component: DonationHistoryComponent },
+
+  
+//   { path: 'donation-item-details/:id', component: DonationItemDetailsComponent },
+//   { path: 'donation-money-details/:id', component: DonationMoneyDetailsComponent },
+//   { path: 'donation-land-details/:id', component: DonationLandDetailsComponent },
+
+  { path: '', redirectTo: '/donation-history', pathMatch: 'full' },
+  { path: '**', redirectTo: '/donation-history' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
