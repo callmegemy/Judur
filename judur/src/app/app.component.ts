f032776e6c35242b2be79598c0e029a05e73d02b
shrouk <shroukeslam676@gmail.com>
2024-09-23 21:35:34 +0300

    testing components

--- judur/src/app/app.component.ts
+++ judur/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';  
 import { RouterModule, RouterOutlet } from '@angular/router'; 
-// import { MainHeaderComponent } from './main-header/main-header.component';
-// import { DonationHistoryComponent } from './donation-history/donation-history.component'; 
+import { DonationHistoryComponent } from './components/donation-history/donation-history.component';
 import { compileNgModule } from '@angular/compiler';
 import { CommonModule } from '@angular/common';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, CommonModule],
+  imports: [RouterModule, RouterOutlet,DonationHistoryComponent , CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
