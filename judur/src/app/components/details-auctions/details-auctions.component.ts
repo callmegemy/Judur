db9c071319303b399120e4807cf1f70870b8bc2a
samyelsayed <samyelsayed754@gmail.com>
2024-09-25 21:06:27 +0300

    A page displaying details of the item being auctioned.

---
+++ judur/src/app/components/details-auctions/details-auctions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-auctions',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-auctions.component.html',
+  styleUrl: './details-auctions.component.css'
+})
+export class DetailsAuctionsComponent {
+
+}
