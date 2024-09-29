1193385240ed70d81d741547d6fad699fa5c4442
Abdelrahman1022 <abdoayman56@yahoo.com>
2024-09-29 12:45:26 +0200

    i made maodifications in sidebar mainly and in other sections , i fixed some errors

--- judur/src/app/components/blog-details/blog-details.component.ts
+++ judur/src/app/components/blog-details/blog-details.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+interface BlogPost {
+  id: number;
+  imgSrc: string;
+  title: string;
+  description: string;
+  author: string;
+  comments: number;
+  content: string;
+}
 
 @Component({
   selector: 'app-blog-details',
@@ -10,30 +21,16 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./blog-details.component.css']
 })
 export class BlogDetailsComponent implements OnInit {
-  mainImage: string = 'assets/img/blog-1.jpg';
-  postTitle: string = 'Lorem ipsum dolor sit amet';
-  postContent: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst. Cras nec sollicitudin eros. Nunc eu enim non turpis sagittis rhoncus consectetur id augue. Mauris dignissim neque felis. Phasellus mollis mi a pharetra cursus. Maecenas vulputate augue placerat lacus mattis, nec ornare risus sollicitudin.';
+  mainImage: string = '';
+  postTitle: string = '';
+  postContent: string = '';
 
   authorImage: string = 'assets/img/author-1.jpg';
   authorName: string = 'Author Name';
   authorBio: string = 'Lorem ipsum dolor sit amet elit. Integer lorem augue purus mollis sapien, non eros leo in nunc. Donec a nulla vel turpis tempor ac vel justo. In hac platea dictumst.';
 
   comments: any[] = [
-    {
-      userImage: 'assets/img/commenter-1.jpeg',
-      userName: 'Josh Dunn',
-      date: new Date('2045-01-01T12:00:00'),
-      content: 'Lorem ipsum dolor sit amet elit. Integer lorem augue purus mollis sapien, non eros leo in nunc. Donec a nulla vel turpis tempor ac vel justo. In hac platea dictumst.',
-      replies: [
-        {
-          userImage: 'assets/img/commenter-2.jpeg',
-          userName: 'Josh Dunn',
-          date: new Date('2045-01-01T12:00:00'),
-          content: 'Lorem ipsum dolor sit amet elit. Integer lorem augue purus mollis sapien, non eros leo in nunc. Donec a nulla vel turpis tempor ac vel justo. In hac platea dictumst.'
-        }
-      ]
-    },
-    // Add more comments...
+    // ... Keep existing comments ...
   ];
 
   newComment: any = {
@@ -46,10 +43,10 @@ export class BlogDetailsComponent implements OnInit {
   searchKeyword: string = '';
 
   recentPosts: any[] = [
-    { image: 'assets/img/blog-1.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur' },
-    { image: 'assets/img/blog-2.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur' },
-    { image: 'assets/img/blog-3.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur' },
-    { image: 'assets/img/blog-2.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur' },
+    { image: 'assets/img/carousel-3.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur', id: 0 },
+    { image: 'assets/img/blog-1.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur', id: 1 },
+    { image: 'assets/img/blog-2.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur', id: 2 },
+    { image: 'assets/img/blog-3.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Judur', id: 3 },
   ];
 
   sidebarImage1: string = 'assets/img/blog-1.jpg';
@@ -57,19 +54,90 @@ export class BlogDetailsComponent implements OnInit {
   activeTab: string = 'featured';
 
   featuredPosts: any[] = [
-    { image: 'assets/img/blog-2.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Web Design' },
-    { image: 'assets/img/blog-3.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Web Design' },
-    { image: 'assets/img/blog-1.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Web Design' },
-    { image: 'assets/img/blog-2.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Web Design' },
-    { image: 'assets/img/blog-3.jpg', title: 'Lorem ipsum dolor sit amet consec adipis elit', author: 'Admin', category: 'Web Design' },
+    { image: 'assets/img/blog-1.jpg', title: 'Featured: Lorem ipsum dolor sit amet', author: 'Admin', category: 'Judur', id: 1 },
+    { image: 'assets/img/blog-2.jpg', title: 'Featured: Consectetur adipiscing elit', author: 'Admin', category: 'Judur', id: 2 },
+    { image: 'assets/img/carousel-3.jpg', title: 'Featured: Sed do eiusmod tempor incididunt', author: 'Admin', category: 'Judur', id: 0 },
+  ];
+
+  popularPosts: any[] = [
+    { image: 'assets/img/blog-2.jpg', title: 'Popular: Ut labore et dolore magna aliqua', author: 'Editor', category: 'Judur', id: 2 },
+    { image: 'assets/img/blog-3.jpg', title: 'Popular: Quis nostrud exercitation ullamco', author: 'Editor', category: 'Judur', id: 3 },
+    { image: 'assets/img/blog-2.jpg', title: 'Popular: Duis aute irure dolor in reprehenderit', author: 'Editor', category: 'Judur', id: 2 },
+  ];
+
+  latestPosts: any[] = [
+    { image: 'assets/img/carousel-3.jpg', title: 'Latest: Excepteur sint occaecat cupidatat', author: 'Writer', category: 'Judur', id: 0 },
+    { image: 'assets/img/blog-1.jpg', title: 'Latest: Non proident, sunt in culpa qui', author: 'Writer', category: 'Judur', id: 1 },
+    { image: 'assets/img/blog-2.jpg', title: 'Latest: Officia deserunt mollit anim id est', author: 'Writer', category: 'Judur', id: 2 },
   ];
 
   textWidgetContent: string = 'Lorem ipsum dolor sit amet elit. Integer lorem augue purus mollis sapien, non eros leo in nunc. Donec a nulla vel turpis tempor ac vel justo. In hac platea nec eros. Nunc eu enim non turpis id augue.';
 
-  constructor() { }
+  blogPosts: BlogPost[] = [
+    {
+      id: 0,
+      imgSrc: "assets/img/carousel-3.jpg",
+      title: "Lorem ipsum dolor sit",
+      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
+      author: "Admin",
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
+    },
+    {
+      id: 1,
+      imgSrc: "assets/img/blog-1.jpg",
+      title: "Consectetur adipiscing elit",
+      description: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu.",
+      author: "Jane Doe",
+      comments: 10,
+      content: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu. Fusce vel turpis in tellus faucibus malesuada. Donec in est et urna faucibus eleifend. Nullam tincidunt, nunc vitae aliquam dapibus, velit dolor convallis velit, vel bibendum nisi nunc vel augue."
+    },
+    {
+      id: 2,
+      imgSrc: "assets/img/blog-2.jpg",
+      title: "Consectetur adipiscing elit",
+      description: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu.",
+      author: "Jane Doe",
+      comments: 10,
+      content: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu. Fusce vel turpis in tellus faucibus malesuada. Donec in est et urna faucibus eleifend. Nullam tincidunt, nunc vitae aliquam dapibus, velit dolor convallis velit, vel bibendum nisi nunc vel augue."
+    },
+    {
+      id: 3,
+      imgSrc: "assets/img/blog-3.jpg",
+      title: "Consectetur adipiscing elit",
+      description: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu.",
+      author: "Jane Doe",
+      comments: 10,
+      content: "Consectetur adipiscing elit. Nulla condimentum tortor sem, in semper nisl bibendum eu. Fusce vel turpis in tellus faucibus malesuada. Donec in est et urna faucibus eleifend. Nullam tincidunt, nunc vitae aliquam dapibus, velit dolor convallis velit, vel bibendum nisi nunc vel augue."
+    },
+    // ... Add more blog posts here ...
+  ];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // Initialize component data, fetch from API if needed
+    this.route.paramMap.subscribe(params => {
+      const id = Number(params.get('id'));
+      this.loadPostDetails(id);
+    });
+  }
+
+  loadPostDetails(id: number): void {
+    const post = this.blogPosts.find(p => p.id === id);
+    if (post) {
+      this.mainImage = post.imgSrc;
+      this.postTitle = post.title;
+      this.postContent = post.content;
+      this.authorName = post.author;
+      // You can add more properties here as needed
+    } else {
+      console.error('Post not found');
+      // Handle the case when the post is not found (e.g., redirect to 404 page)
+    }
+  }
+
+  onPostClick(postId: number): void {
+    this.loadPostDetails(postId);
   }
 
   onSubmitComment(): void {
--- judur/src/app/components/blog/blog.component.ts
+++ judur/src/app/components/blog/blog.component.ts
@@ -3,92 +3,97 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 interface BlogPost {
-  img: string;
+  id: number;
+  imgSrc: string;
   title: string;
   description: string;
   author: string;
   comments: number;
+  content: string;
 }
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [CommonModule , RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
-  blogPosts = [
+  blogPosts: BlogPost[] = [
     {
-      imgSrc: "assets/img/blog-1.jpg",
+      id: 0,
+      imgSrc: "assets/img/carousel-3.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
     },
     {
-      imgSrc: "assets/img/blog-2.jpg",
-      title: "Lorem ipsum dolor sit",
-      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
-      author: "Admin",
-      comments: 15
-    },
-    {
+      id: 3,
       imgSrc: "assets/img/blog-3.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
     },
     {
+      id: 1,
       imgSrc: "assets/img/blog-1.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
     },
     {
+      id: 2,
       imgSrc: "assets/img/blog-2.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
     },
     {
-      imgSrc: "assets/img/blog-2.jpg",
+      id: 0,
+      imgSrc: "assets/img/carousel-3.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
     },
     {
-      imgSrc: "assets/img/blog-2.jpg",
-      title: "Lorem ipsum dolor sit",
-      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
-      author: "Admin",
-      comments: 15
-    },
-    {
-      imgSrc: "assets/img/blog-2.jpg",
-      title: "Lorem ipsum dolor sit",
-      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
-      author: "Admin",
-      comments: 15
-    },
-    {
-      imgSrc: "assets/img/blog-2.jpg",
-      title: "Lorem ipsum dolor sit",
-      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
-      author: "Admin",
-      comments: 15
-    },
-    {
+      id: 3,
       imgSrc: "assets/img/blog-3.jpg",
       title: "Lorem ipsum dolor sit",
       description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
       author: "Admin",
-      comments: 15
-    }
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
+    },
+    {
+      id: 1,
+      imgSrc: "assets/img/blog-1.jpg",
+      title: "Lorem ipsum dolor sit",
+      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
+      author: "Admin",
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
+    },
+    {
+      id: 2,
+      imgSrc: "assets/img/blog-2.jpg",
+      title: "Lorem ipsum dolor sit",
+      description: "Lorem ipsum dolor sit amet elit. Neca pretim miura bitur facili ornare velit non vulpte liqum metus tortor",
+      author: "Admin",
+      comments: 15,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, lorem eu eleifend bibendum, augue purus mollis sapien, non rhoncus eros leo in nunc. Donec a nulla vel turpis consectetur tempor ac vel justo. In hac habitasse platea dictumst."
+    },
+    // Add more blog posts here...
   ];
 
   currentPage: number = 1;
@@ -97,5 +102,5 @@ export class BlogComponent {
   get paginatedPosts() {
     const startIndex = (this.currentPage - 1) * this.pageSize;
     return this.blogPosts.slice(startIndex, startIndex + this.pageSize);
-}
+  }
 }
